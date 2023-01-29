f1144b805f467812d6e91711faab792e118c5c0e
kieranvenison <venison@hey.com>
2023-01-29 18:08:55 +0000

    fix: renamed pages to use new*.page.txt naming

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'api.ts', 'api.js', 'route.ts', 'route.js'],
 }
 
 module.exports = nextConfig
--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import HomePage from '../../pages/index';
+import HomePage from '../index.page';
 
 describe('HomePage', () => {
     it('renders without crashing', () => {
