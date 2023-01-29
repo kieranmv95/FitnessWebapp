ca09ae5f6ad532b68b36693e6e080a765527d408
kieranvenison <venison@hey.com>
2023-01-29 18:46:27 +0000

    feat: added login page and SEO

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Login() {
   return (
     <>
       <Head>
-        <title>Fitness App | Home</title>
+        <title>Fitness App | Login</title>
         <meta name="description" content="Fitness App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
