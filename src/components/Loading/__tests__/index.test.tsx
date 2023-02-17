5cee07c0e9f77b66b54a4baf1b476d53596f3eff
kieranvenison <venison@hey.com>
2023-02-17 13:54:41 +0000

    test: loading component

---
+++ src/components/Loading/__tests__/index.test.tsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react'§
+import Loading from '../index'
+
+describe('<PrivateRoute />', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<Loading />)
+    expect(container).toBeInTheDocument()
+  })
+
+  it('displays Loading', () => {
+    const { getByText } = render(<Loading />)
+    expect(getByText('Loading...')).toBeInTheDocument()
+  })
+})
