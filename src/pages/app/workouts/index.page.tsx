0b2856d88a2e64e39843d93447d6654c1e670b95
kieranvenison <venison@hey.com>
2023-02-17 22:13:09 +0000

    feat: added workout page and new workout page

--- src/pages/app/workouts/index.page.tsx
+++ src/pages/app/workouts/index.page.tsx
@@ -1,7 +1,11 @@
 import Seo from '@/components/Seo'
 import PrivateRoute from '@/components/PrivateRoute'
+import Button from '@/components/Button'
+import { useRouter } from 'next/router'
 
 export default function Workouts() {
+  const { push } = useRouter()
+
   return (
     <PrivateRoute>
       <Seo
@@ -10,6 +14,7 @@ export default function Workouts() {
       />
       <div className="text-zinc-800 p-6">
         <h1 className="font-semibold text-3xl mb-4">Workouts</h1>
+        <Button onClick={() => push('/app/workouts/new')}>New Workout</Button>
       </div>
     </PrivateRoute>
   )
